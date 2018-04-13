11d12c3258f33c3913a4fec801d2f6d98de11e5c
wenweiping999 <wenweiping999@163.com>
2018-04-13 16:39:01 +0800

    modifed hello.go

--- hello.go
+++ hello.go
@@ -5,11 +5,11 @@ import "os"
 func main(){
 
   fmt.Fprint(os.Stdout,"my name is ","wenweiping go \n");
-
+  fmt.Println("this is branch: go_infrastructure_20180413")
   fmt.Println("886")
 }
 
 
-// this git clinet lenovo  branch master 
+// this git clinet lenovo  branch 
 
 
