30b5304a30cdda3b22a9466c31b79eee477329aa
webing <wenweiping913@163.com>
2018-04-14 13:39:18 +0800

    insert some codes

--- hello.go
+++ hello.go
@@ -6,6 +6,7 @@ func main(){
 
   fmt.Fprint(os.Stdout,"my name is ","wenweiping go \n");
 
+  fmt.Println("new line insert")
   fmt.Fprint(os.Stdout,"I am  arch master: ","wenweiping arch-dalsv\n")
   fmt.Println("this is go_infrastructure_20180414 version")
   fmt.Println("886")
